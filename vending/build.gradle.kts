024f1d098b79240d809d64ffa0591f250b22100f
Daniil <ksusonic@yandex.ru>
2023-04-14 19:40:07 +0300

    working scheme

--- vending/build.gradle.kts
+++ vending/build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0-RC"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
     application
 }
 
@@ -15,9 +16,11 @@ repositories {
 val ktor_version = "2.2.4"
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-client-websockets:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
     implementation("io.ktor:ktor-network:$ktor_version")
     implementation("org.slf4j:slf4j-simple:2.0.5")
